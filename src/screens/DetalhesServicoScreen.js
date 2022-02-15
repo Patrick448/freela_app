e7535b771c6ed7e7c7ae681db438d3a783798017
Luciana Nascimento <luciananssantana@gmail.com>
2022-02-15 15:14:08 -0300

    detalhes

--- src/screens/DetalhesServicoScreen.js
+++ src/screens/DetalhesServicoScreen.js
@@ -1,17 +1,17 @@
 import { Feather, MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
 import React, { useState, useEffect, useCallback } from 'react';
 import {
-	StyleSheet,
-	Text,
-	View,
-	TouchableOpacity,
-	Image,
-	KeyboardAvoidingView,
-	StatusBar,
-	Alert,
-	Dimensions,
-	ActivityIndicator,
-	Platform,
+    StyleSheet,
+    Text,
+    View,
+    TouchableOpacity,
+    Image,
+    KeyboardAvoidingView,
+    StatusBar,
+    Alert,
+    Dimensions,
+    ActivityIndicator,
+    Platform,
 } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import LineSeparator from '../components/LineSeparator';
@@ -24,19 +24,20 @@ import {useDispatch, useSelector} from "react-redux";
 import Contrato from "../model/Contrato";
 import * as contratosActions from "../store/actions/contratosActions"
 
-const DetalhesServicoScreen = (props) => {
+const DetalhesServicoScreen = ({route, navigation}) => {
 
     const dispatch = useDispatch();
 
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState();
+    let services;
 
     const contratar = async (values) => {
         console.log('values: ')
         console.log(values);
 
         const contrato = new Contrato(values.id, 2, values.preco / 2,
-            props.procurado? user.id : values.anuncianteId, props.procurado? values.anuncianteId : user.id)
+            route.params.procurado? user.id : values.anuncianteId, route.params.procurado? values.anuncianteId : user.id)
         console.log(contrato);
 
 
@@ -45,7 +46,7 @@ const DetalhesServicoScreen = (props) => {
             await dispatch(contratosActions.registrarContrato(contrato));
             setLoading(false);
             Alert.alert("Contrato registrado com sucesso!");
-            props.navigation.pop(1);
+            navigation.pop(1);
         } catch (error) {
             setLoading(false);
             Alert.alert("Falha ao registrar contrato");
@@ -54,119 +55,118 @@ const DetalhesServicoScreen = (props) => {
     }
 
 
-    let services;
-    if (props.procurado) {
-        services = useSelector((state) => state.servicos.servicosProcurados);
+    if (route.params.procurado) {
+        services = useSelector((state) => state.servicos.servicosProcurados.lista);
     } else {
-        services = useSelector((state) => state.servicos.servicosOferecidos);
+        services = useSelector((state) => state.servicos.servicosOferecidos.lista);
     }
 
     const user = useSelector((state) => state.user.currentUser);
 
-    const currentService = services.find(element => element.id == props.idItem);
+    const currentService = services.find(element => element.id === route.params.idItem);
 
-	return (
-		<View style={{flex: 1, backgroundColor: Colors.white}}>
-			<View style={{paddingTop: 40, paddingLeft: 15, justifyContent: 'center', alignContent: 'center'}}>
-			<Text style={{fontSize:18, fontFamily:'red-hat-medium'}}>{currentService.titulo}</Text>
-      		</View>
-			<Image>
-			</Image>
-			<View style={styles.description}>
-				<Text style={styles.title}>
+    return (
+        <View style={{flex: 1, backgroundColor: Colors.white}}>
+            <View style={{paddingTop: 40, paddingLeft: 15, justifyContent: 'center', alignContent: 'center'}}>
+                <Text style={{fontSize:18, fontFamily:'red-hat-medium'}}>{currentService.titulo}</Text>
+            </View>
+            <Image>
+            </Image>
+            <View style={styles.description}>
+                <Text style={styles.title}>
                     {currentService.descricao}
-				</Text>
-			</View>
-			<LineSeparator></LineSeparator>
-			<View>
-            <View style={styles.containerText}>
-
-                <View style={{...styles.userIcon, marginLeft: 10}}>
-                    <FontAwesome 
-                        name="user" 
-                        size={20} 
-                        color="#808080"
-                    />
-                </View>
-                <View style={styles.containerTitle}>
-                    <Text style={styles.title}>{currentService.anunciante}</Text>
-                </View>
-                
+                </Text>
             </View>
-        	</View>
-			<View>
-            <View style={styles.containerText}>
+            <LineSeparator></LineSeparator>
+            <View>
+                <View style={styles.containerText}>
 
-                <View style={styles.userIcon}>
-                    <MaterialIcons 
-                        name="attach-money" 
-                        size={22} 
-                        color="#808080"
-                    />
+                    <View style={{...styles.userIcon, marginLeft: 10}}>
+                        <FontAwesome
+                            name="user"
+                            size={20}
+                            color="#808080"
+                        />
+                    </View>
+                    <View style={styles.containerTitle}>
+                        <Text style={styles.title}>{currentService.anunciante}</Text>
+                    </View>
+
                 </View>
-                <View style={styles.containerTitle}>
-                    <Text style={styles.title}>{currentService.preco}</Text>
-                </View>
-                
             </View>
-        	</View>
-			<View>
-            <View style={styles.containerText}>
+            <View>
+                <View style={styles.containerText}>
 
-                <View style={styles.userIcon}>
-                    <Feather 
-                        name="clock" 
-                        size={20} 
-                        color="#808080"
-                    />
+                    <View style={styles.userIcon}>
+                        <MaterialIcons
+                            name="attach-money"
+                            size={22}
+                            color="#808080"
+                        />
+                    </View>
+                    <View style={styles.containerTitle}>
+                        <Text style={styles.title}>{currentService.preco}</Text>
+                    </View>
+
                 </View>
-                <View style={styles.containerTitle}>
-                    <Text style={styles.title}> Horário a combinar</Text>
-                </View>
-                
             </View>
-        	</View>
-			<View>
-            <View style={styles.containerText}>
+            <View>
+                <View style={styles.containerText}>
 
-                <View style={styles.userIcon}>
-                    <MaterialCommunityIcons 
-                        name="map-marker" 
-                        size={20} 
-                        color="#808080"
-                    />
+                    <View style={styles.userIcon}>
+                        <Feather
+                            name="clock"
+                            size={20}
+                            color="#808080"
+                        />
+                    </View>
+                    <View style={styles.containerTitle}>
+                        <Text style={styles.title}> Horário a combinar</Text>
+                    </View>
+
                 </View>
-                <View style={styles.containerTitle}>
-                    <Text style={styles.title}>{currentService.local}</Text>
-                </View>
-                
             </View>
-        	</View>
+            <View>
+                <View style={styles.containerText}>
 
-			<View style={{justifyContent: "center", alignItems: "center", marginTop: 40}}>
-				<FormButton onPress={contratar(currentService)} title={props.procurado ? "Oferecer" : "Contratar"} colorBack={Colors.secondaryColor} width="30%"></FormButton>
-			</View>
+                    <View style={styles.userIcon}>
+                        <MaterialCommunityIcons
+                            name="map-marker"
+                            size={20}
+                            color="#808080"
+                        />
+                    </View>
+                    <View style={styles.containerTitle}>
+                        <Text style={styles.title}>{currentService.local}</Text>
+                    </View>
+
+                </View>
+            </View>
+
+            <View style={{justifyContent: "center", alignItems: "center", marginTop: 40}}>
+                <FormButton onPress={()=>{contratar(currentService)}} title={route.params.procurado ? "Oferecer" : "Contratar"} colorBack={Colors.secondaryColor} width="30%"></FormButton>
+            </View>
         </View>
-	);
+    );
 };
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-		backgroundColor: Colors.primaryColor,
-	},
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: Colors.primaryColor,
+    },
     logoContainer: {
-		alignItems: 'center',
-	},
-	logo: {
-		width: Dimensions.get('window').width * 0.8,
-		height: Dimensions.get('window').height * 0.15,
-		resizeMode: 'contain',
-		marginBottom: 10,
-	},
-	containerText: {
+        alignItems: 'center',
+    },
+    logo: {
+        width: Dimensions.get('window').width * 0.8,
+        height: Dimensions.get('window').height * 0.15,
+        resizeMode: 'contain',
+        marginBottom: 10,
+    },
+    containerText: {
         marginTop: 5,
         flexDirection: "row",
         justifyContent: "flex-start",
@@ -178,7 +178,7 @@ const styles = StyleSheet.create({
     containerTitle: {
         justifyContent: "flex-end",
         alignItems: "flex-end",
-		marginVertical: 10
+        marginVertical: 10
     },
     title: {
         color: "#808080",
@@ -189,11 +189,11 @@ const styles = StyleSheet.create({
         flexDirection: "row",
         justifyContent: 'flex-start',
         marginHorizontal: 8,
-		marginVertical: 10
+        marginVertical: 10
     },
-	description: {
-		margin: 15
-	}
+    description: {
+        margin: 15
+    }
 });
 
 export default DetalhesServicoScreen;
